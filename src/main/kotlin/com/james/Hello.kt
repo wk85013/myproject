eafef3568a0773943f8bd11432b54dc6e63d76f7
JamesHuang <wk85013@hotmail.com>
2019-01-01 22:03:46 +0800

    ss

---
+++ src/main/kotlin/com/james/Hello.kt
@@ -0,0 +1,15 @@
+package com.james
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human() { 
+    fun hello() {
+        println("hello kotlin")
+    }
+
+}
